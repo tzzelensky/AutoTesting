4569cf929951de5bf257e187ac01c3f00dcd9fe2
tzzelen <tzzelensk@gmail.com>
2026-01-08 21:21:01 +0500

    Merge remote-tracking branch 'origin/main'

--- test/test_guest_admin.py
+++ test/test_guest_admin.py
@@ -2,7 +2,7 @@ import pytest
 
 from pages.guests_page import GuestsPage
 
-
+@pytest.flaky(reruns=2, retry_delay=5)
 @pytest.mark.first_test
 def test_find_guest(guests_page: GuestsPage):
     guests_page.visit('https://k8s-devtest1.goulash.tech/guests/guests/admin')
